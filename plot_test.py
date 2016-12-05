ba9b5cc3c7f3a86534177d87ca4ec99d1f4b71a1
Gi-yong Kwon <handgear05@gmail.com>
2016-12-05 15:11:25 +0900

    Add range circles of router

--- plot_test.py
+++ plot_test.py
@@ -1,11 +1,16 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Circle
+from matplotlib.lines import Line2D
+
+
 class Setting:
     MAX_ROUTER_NUM = 1500
     MAX_ROUTER_RANGE = 100
     router_num = 20
-    router_range = 3
+    router_range = 30
 
 class Router:
     def __init__(self):
@@ -18,24 +23,29 @@ class Router:
     #     self.result += num
     #     return self.result
 
-router1 = Router()
 setting = Setting()
 
+#get router number and router range
+# while True :
+#     setting.router_num = input("input number of routers(1<x<1500): ")
+#     if setting.router_num < setting.MAX_ROUTER_NUM and setting.router_num > 1 :
+#         setting.router_range = input("input covering range of each router(1<x<100): ")
+#         if setting.router_range < setting.MAX_ROUTER_RANGE and setting.router_range > 0:
+#             break
 
-# router_num = 3 #get by user input (later)
-while True :
-    router_num = input("input number of routers(1<x<1500): ");
-    if router_num < 1500 and router_num > 1 :
-        break
+
 #make router objects
 router_list = [Router() for i in range(setting.router_num)]
 
 
 plt.axis([0, 1000, 0, 1000])
-# plt.plot(router1.x, router1.y,'bo')
-for i in range(router_num) :
-    plt.plot(router_list[i].x, router_list[i].y,'bo')
-plt.ylabel('some numbers')
+
+for i in range(setting.router_num) :
+    plt.plot(router_list[i].x, router_list[i].y,'bo') #draw router
+    circle = plt.Circle((router_list[i].x, router_list[i].y), radius=setting.router_range, alpha=0.3, fc='blue')
+    plt.gca().add_patch(circle)
+
+
 plt.show()
 
 
