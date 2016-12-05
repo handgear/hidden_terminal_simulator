d78b0e3f72757c14b2d136816461172f034e0977
Gi-yong Kwon <handgear05@gmail.com>
2016-12-05 12:56:59 +0900

    Add user input for router number

--- plot_test.py
+++ plot_test.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 class Router:
     def __init__(self):
+        #do it need to avoid router in same place?
+        #place router in random spot
         self.x = random.randrange(1, 1000)
         self.y = random.randrange(1, 1000)
 
@@ -14,13 +16,17 @@ class Router:
 router1 = Router()
 
 # router_num = 3 #get by user input (later)
-router_num = input("input number of routers(1<x<3000): ");
-router_list = [Router() for i in range(router_num)]
+while True :
+    router_num = input("input number of routers(1<x<1500): ");
+    if router_num < 1500 and router_num > 1 :
+        break
+
+router_list = [Router() for i in range(router_num)] #make router objects
 
 # plt.plot([1,2,3,4],'bo')
 plt.axis([0, 1000, 0, 1000])
 # plt.plot(router1.x, router1.y,'bo')
-for i in range(router_num):
+for i in range(router_num) :
     plt.plot(router_list[i].x, router_list[i].y,'bo')
 plt.ylabel('some numbers')
 plt.show()
